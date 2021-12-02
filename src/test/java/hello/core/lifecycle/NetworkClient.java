98782dfd0bd3a1ae2903348d191479e9c3011ec4
mssdfg0708 <mssdfg0708@naver.com>
2021-12-02 18:41:37 +0900

    프로토타입 스코프

---
+++ src/test/java/hello/core/lifecycle/NetworkClient.java
@@ -0,0 +1,44 @@
+package hello.core.lifecycle;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
+public class NetworkClient {
+
+    private String url;
+
+    public NetworkClient() {
+        System.out.println("Call Constructor = " + url);
+        connect();
+        call("Default Connect Message");
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public void connect() {
+        System.out.println("connect = " + url);
+    }
+
+    public void call(String message) {
+        System.out.println("call = " + url + "message = " + message);
+    }
+
+    public void disconnect() {
+        System.out.println("disconnect = " + url);
+    }
+
+    @PostConstruct
+    public void init() {
+        System.out.println("NetworkClient.init");
+        connect();
+        call("Default Connect Message");
+    }
+
+    @PreDestroy
+    public void close() {
+        System.out.println("NetworkClient.close");
+        disconnect();
+    }
+}
